79221930143a9db6334aff059d3e3692853456ce
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-12 01:56:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,9 @@ def printer(attendees)
     puts batch_badge_creator(attendees)
   end
 
-  
-    
+  assign_rooms(attendees).each do |attendee|
+    puts assign_rooms(attendees)
+  end
+ 
   
 end
